7aaebcf54bba89d56ada527a528e159af940dcdb
Sun5hine <af5kh@yahoo.com>
2019-08-31 18:37:15 +0100

    index for portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ puts "10 blogs posts created"
 end
 puts "5 skills created"
 
-9.times do |portfolio|
+9.times do |portfolio_items|
   Portfolio.create!(
-    title: "portfolio title #{portfolio}",
+    title: "portfolio title #{portfolio_items}",
     subtitle: "my great service",
     body: "it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
     main_image:"https://via.placeholder.com/300.png/" ,
