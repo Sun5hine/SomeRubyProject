225c35d3aedbe8824e812b70a64c04b4439739c9
Sun5hine <af5kh@yahoo.com>
2019-09-03 14:51:12 +0100

    new and create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,19 +4,28 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
-  def show
-  end
 
   def new
+    @portfolio_item = Portfolio.new
   end
 
   def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
+
 
   def edit
   end
 
   def destroy
-  end 
+  end
 
 end
