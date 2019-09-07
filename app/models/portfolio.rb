6067b1622ecd61f760f96cb4f96a609f4f0d2084
Sun5hine <af5kh@yahoo.com>
2019-09-07 19:23:27 +0100

    concern file

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
 validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
 def self.angular
@@ -10,8 +11,8 @@ scope :whatever_shit, ->{ where(subtitle: "Angular")}
 after_initialize :set_defaults
 
 def set_defaults
- self.main_image ||= "https://via.placeholder.com/250"
- self.thumb_image ||= "https://via.placeholder.com/200"
+ self.main_image ||= Placeholder.image_generator(height:250)
+ self.thumb_image ||= Placeholder.image_generator(height:200)
 end
 
 end
