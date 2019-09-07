aa3e2b476f844590172abe0feac83f5bca88350f
Sun5hine <af5kh@yahoo.com>
2019-09-07 15:29:35 +0100

    custome scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,10 @@
 class Portfolio < ApplicationRecord
+validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
-  validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+def self.angular
+  where(subtitle: "Angular")
+end
+
+scope :whatever_shit, ->{ where(subtitle: "Angular")}
+
 end
