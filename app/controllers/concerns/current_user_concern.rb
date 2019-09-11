c464088f588f8a3fbe40230964937aa27c2758ef
Sun5hine <af5kh@yahoo.com>
2019-09-11 19:29:50 +0100

    OpenStruct

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,10 @@
+module CurrentUserConcern
+
+  extend ActiveSupport::Concern
+
+  def current_user
+   super || OpenStruct.new(name:"Guest User", first_name: "Guest", last_name:"User", email:"gibbersish.com")
+  end
+
+
+end
